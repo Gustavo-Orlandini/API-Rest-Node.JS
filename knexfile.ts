03831be84dddf85673a034cca2414c255186a987
*Gustavo <guga77sugu@hotmail.com>
2023-08-13 00:25:05 -0300

    Create a migration, to add session in other migration!

--- knexfile.ts
+++ knexfile.ts
@@ -6,4 +6,4 @@ export default config
 
 // De acordo com uma convenção, é necessário criar esse arquivo e trazer o config
 // do database pra cá, para o knex reconhecer as configurações do nosso banco.
-//Tal medida se da pois ele só entende Javascript, e não Typescript
+//Tal medida se da pois ele só entende Javascript, e não Typescript 
